ffe59b0434af9096f202f69ad5d34d31e8b44634
zixingdeng <zixingdeng@tencent.com>
2024-07-08 11:09:37 +0800

    add node setup cleanup script

--- unittest/script/linux/nodeSetup.sh
+++ unittest/script/linux/nodeSetup.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
 set -exu -o pipefail
+
+if [ -n "$TESTSOLAR_TTP_SETUP_CMDLINE" ]; then
+    echo "Executing node setup command: $TESTSOLAR_TTP_SETUP_CMDLINE"
+    eval "$TESTSOLAR_TTP_SETUP_CMDLINE"
+fi
