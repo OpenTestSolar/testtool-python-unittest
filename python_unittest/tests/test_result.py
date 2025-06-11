b915e88e12965ae58c1752628e40e3a14ec3f8fd
zixingdeng <zixingdeng@tencent.com>
2025-06-11 14:58:53 +0800

    修复解析初始化失败输出文件问题

--- python_unittest/tests/test_result.py
+++ python_unittest/tests/test_result.py
@@ -10,3 +10,15 @@ def test_parse_test_report():
         proj_path=testdata_dir, xml_file=os.path.join(testdata_dir, "xml_results.xml")
     )
     assert len(results) == 30
+
+
+def test_parse_failed_report():
+    results = parse_test_report(
+        proj_path=testdata_dir,
+        xml_file=os.path.join(testdata_dir, "failed_results.xml"),
+    )
+    assert len(results) == 1
+    assert (
+        results[0].Test.Name
+        == "setUpClass (example.order_process.test_ct_cn_order_approval.TestExample)"
+    )
